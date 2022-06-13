eb8fa2ede6ab98a413c0890e53420451fa52c5bc
PatrykJarzynka <patryk.jarzynka1234@gmail.com>
2022-06-13 20:57:13 +0200

    Poprawiono zapis styli

--- src/components/landing/LandingInfo.js
+++ src/components/landing/LandingInfo.js
@@ -2,14 +2,11 @@ import styles from "./landing.module.css";
 import React from "react";
 
 function LandingInfo() {
-  const infoStyles = styles.info;
-  const landingHeaderStyles = styles.landingHeader;
-  const landingParagraphStyles = styles.landingParagraph;
 
   return (
-    <div className={infoStyles}>
-      <h1 className={landingHeaderStyles}>Questify</h1>
-      <p className={landingParagraphStyles}>
+    <div className={styles.info}>
+      <h1 className={styles.landingHeader}>Questify</h1>
+      <p className={styles.landingParagraph}>
         Questify will turn your life into a thrilling game full of amazing
         quests and exciting challenges.
       </p>
