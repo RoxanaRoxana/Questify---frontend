3c18b00a56e86a0c27a7e781a80488ad95c4cdb0
RoxanaRoxana <roxana0804@gmail.com>
2022-06-15 17:41:19 +0200

    basic route

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "../navbar/Navbar";
+import {
+  TodayContainer,
+  TomorrowContainer,
+  DoneContainer,
+} from "../../containers";
+import styles from './Layout.module.css'
+
+const Layout = (props) => {
+  return (
+    <>
+      <Navbar />
+      <div className={styles.section_container}>
+        <TodayContainer />
+        <TomorrowContainer />
+        <DoneContainer />
+      </div>
+      {/* <main>{ props.children}</main> */}
+    </>
+  );
+};
+
+export default Layout;
