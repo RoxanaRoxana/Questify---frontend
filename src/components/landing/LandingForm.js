3e892f891fa3943f69638a630976b6e43a2ac05b
RoxanaRoxana <roxana0804@gmail.com>
2022-06-15 18:02:30 +0200

    fix

--- src/components/landing/LandingForm.js
+++ src/components/landing/LandingForm.js
@@ -3,7 +3,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function LandingForm() {
-
+  const navigate = useNavigate();
 
   const handleClick = () => {
    navigate('/main')
